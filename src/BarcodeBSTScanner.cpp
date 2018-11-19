b41902ea6af3f7ed9c87147678b0421a25767020
Evan <Evan@Evans-MacBook-Pro.local>
2018-11-19 12:58:49 -0800

    V.1

--- src/BarcodeBSTScanner.cpp
+++ src/BarcodeBSTScanner.cpp
@@ -5,6 +5,15 @@
  *      Author: Evan
  */
 
+#include <iostream>
+#include <time.h>
+#include <sstream>
+#include "BinaryTree.h"
+#include "BarcodeIndex.h"
 
+using namespace std;
 
+int main()
+{
 
+}
---
+++ src/BarcodeIndex.h
@@ -0,0 +1,19 @@
+/*
+ * BarcodeIndex.h
+ *
+ *  Created on: Nov 19, 2018
+ *      Author: Evan
+ */
+
+#ifndef BARCODEINDEX_H_
+#define BARCODEINDEX_H_
+
+#include <string>
+
+struct map
+{
+	long key;
+	string val;
+};
+
+#endif /* BARCODEINDEX_H_ */
