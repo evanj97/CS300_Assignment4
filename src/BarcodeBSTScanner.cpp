0b346f981349e2a6c446642fb2f8965d8bac6b52
Evan <Evan@Evans-MBP.hsd1.wa.comcast.net>
2018-11-19 22:09:14 -0800

    V0.1.1

--- src/BarcodeBSTScanner.cpp
+++ src/BarcodeBSTScanner.cpp
@@ -5,15 +5,14 @@
  *      Author: Evan
  */
 
-#include <iostream>
-#include <time.h>
-#include <sstream>
-#include "BinaryTree.h"
 #include "BarcodeIndex.h"
 
 using namespace std;
 
 int main()
 {
+
+
+
 
 }
--- src/BarcodeIndex.h
+++ src/BarcodeIndex.h
@@ -9,11 +9,32 @@
 #define BARCODEINDEX_H_
 
 #include <string>
+#include <iostream>
+#include <time.h>
+#include <sstream>
+#include "BinaryTree.h"
 
 struct map
 {
 	long key;
 	string val;
 };
+
+class BarcodeIndex
+{
+private:
+
+	BinaryTree<map>* tree;
+
+public:
+
+	BarcodeIndex();
+
+};
+
+BarcodeIndex::BarcodeIndex()
+{
+	tree = new BinaryTree<map>;
+}
 
 #endif /* BARCODEINDEX_H_ */
